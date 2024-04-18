d60d11cc9ad986e57a28d773484b314aa243c9d9
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-18 15:17:00 -0400

    doc: att

--- exercito.js
+++ exercito.js
@@ -22,30 +22,12 @@ async function main(texto) {
 }
 
 async function scrap() {
-    const response = await axios.get("https://ingresso.ufms.br/publicacao/movimentacao-interna/");
+    const response = await axios.get("https://9rm.eb.mil.br/index.php/oficial-tecnico-temporario");
     const $ = cheerio.load(response.data);
-    const countries = $('.title').map((i, item) => ({
+    const countries = $('p').map((i, item) => ({
         texto: $(item).text().trim()
     })).get();
-    
-    const ano = new Date().getFullYear().toString();
-    let textoSemestre1 = "";
-    let textoSemestre2 = "Aguardando...";
-
-    for (let i = 0; i < countries.length && i < 5; i++) {
-        const semestre = countries[i].texto.split(' – ')[1].split(' ')[0];
-        const anoSemestre = countries[i].texto.split(' – ')[1].split(' ')[3];
-        
-        if (anoSemestre === ano) {
-            if (semestre.includes("1")) {
-                textoSemestre1 = countries[i].texto;
-            } else if (semestre.includes("2")) {
-                textoSemestre2 = countries[i].texto;
-            }
-        }
-    }
-    // Envie o e-mail aqui, depois de processar todas as informações.
-    main(`${textoSemestre2}\n\n${textoSemestre1}`);
+    console.log(countries)
 }
 
 scrap();
