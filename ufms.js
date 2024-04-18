fa1ff167c59e53784da661e43f0bece6014d6f97
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-18 09:07:43 -0400

    doc: att

--- ufms.js
+++ ufms.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+var datetime = new Date();
 const countries = [];
 
 async function scrap(){
@@ -10,7 +11,16 @@ async function scrap(){
         (i, item) => countries.push({texto: $(item).text().trim()
         })
     );
-    console.log(countries)
+    
+    ano = datetime.toISOString().slice(0,10).split('-')[0]
+    mes = datetime.toISOString().slice(0,10).split('-')[1]
+    dia = datetime.toISOString().slice(0,10).split('-')[2]
+    
+    for (let i = 0; i < countries.length; i++) {
+        
+        
+    }
+    
 }
  
 scrap();
