57923a0b1ae2dbde585c9401090be5294be0095e
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-18 08:57:22 -0400

    doc: att

--- ufms.js
+++ ufms.js
@@ -6,8 +6,8 @@ const countries = [];
 async function scrap(){
     const response = await axios.get("https://ingresso.ufms.br/publicacao/movimentacao-interna/");
     const $ = cheerio.load(response.data);
-    $('.glyphicon glyphicon-check').each(
-        (i, item) => countries.push({name: $(item).text().trim()
+    $('.title').each(
+        (i, item) => countries.push({texto: $(item).text().trim()
         })
     );
     console.log(countries)
