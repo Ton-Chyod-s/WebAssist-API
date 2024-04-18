4210a7b01a5fd38706271130f01183c00584f8cd
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-18 14:19:40 -0400

    doc: att

--- ufms.js
+++ ufms.js
@@ -7,6 +7,7 @@ const countries = [];
 let textoSemestre1 = ""
 let textoSemestre2 = "Aguardando..."
 
+
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
@@ -26,33 +27,33 @@ async function main(texto) {
     console.log('Email enviado!!')
 }
 
-async function scrap(){
+async function scrap() {
     const response = await axios.get("https://ingresso.ufms.br/publicacao/movimentacao-interna/");
     const $ = cheerio.load(response.data);
-    $('.title').each(
-        (i, item) => countries.push({texto: $(item).text().trim()
-        })
-    );
+    const countries = $('.title').map((i, item) => ({
+        texto: $(item).text().trim()
+    })).get();
     
-    ano = datetime.toISOString().slice(0,10).split('-')[0]
-    for (let i = 0; i < countries.length; i++) {
-        if (i < 5) {
-        Semestre = countries[i]['texto'].split(' – ')[1]
-        countSemestre = countries[i]['texto'].split(' – ')[1].split(' ').length
-        if (countSemestre = 4) {
-            semestre = Semestre.split(' ')[0]
-            anoSemestre = Semestre.split(' ')[countSemestre - 1]
-            if (anoSemestre == ano) {
-                if (semestre.includes("1")) {
-                    textoSemestre1 = countries[i]['texto']
-                } else if (semestre.includes("2")) {
-                    textoSemestre2 = countries[i]['texto']
-                }  
-                main(`${textoSemestre2}\n\n${textoSemestre1}`)
+    const ano = new Date().getFullYear().toString();
+    let textoSemestre1 = "";
+    let textoSemestre2 = "Aguardando...";
+
+    for (let i = 0; i < countries.length && i < 5; i++) {
+        const semestre = countries[i].texto.split(' – ')[1].split(' ')[0];
+        const anoSemestre = countries[i].texto.split(' – ')[1].split(' ')[3];
+        
+        if (anoSemestre === ano) {
+            if (semestre.includes("1")) {
+                textoSemestre1 = countries[i].texto;
+            } else if (semestre.includes("2")) {
+                textoSemestre2 = countries[i].texto;
             }
         }
-        }
     }
+
+    // Envie o e-mail aqui, depois de processar todas as informações.
+    main(`${textoSemestre2}\n\n${textoSemestre1}`);
 }
 
 scrap();
+
