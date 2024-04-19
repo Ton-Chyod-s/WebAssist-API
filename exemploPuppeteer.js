de9e896ffc607a387a8966de4aa1e3292cc92456
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-19 11:25:45 -0400

    doc: att

--- exemploPuppeteer.js
+++ exemploPuppeteer.js
@@ -8,11 +8,16 @@ const puppeteer = require('puppeteer');
   await page.goto('https://www.spdo.ms.gov.br/diariodoe');
 
   // Preencher um campo
-  await page.locator('input').fill('lol');
-
-
-  // await page.locator('input').click();
+  await page.type('[id="Filter_Texto"]', 'Klayton Chrysthian Oliveira Dias');
+  // clicar
+  await page.locator('button').click();
   
+  // 'tbody'
+  await page.waitForSelector('tbody');
+  const planilhaHTML = await page.$eval('tbody', element => element.outerHTML);
+  for (let i = 0; i < planilhaHTML.length; i++) {
+    console.log(planilhaHTML[i])
+  }
 
   `
   // Aguardar um pouco
