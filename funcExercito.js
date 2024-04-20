1f47f684bb634fb4e7cc6b9e4620b56f925b98dc
Klay <hix_x@hotmail.com>
2024-04-20 09:27:06 -0400

    doc: att

--- funcExercito.js
+++ funcExercito.js
@@ -1,10 +1,10 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { main } = require('./mandarEmail');
+
 let texto = "Aguardando..."
 
-async function scrap() {
+async function Exercito() {
     const response = await axios.get("https://9rm.eb.mil.br/index.php/oficial-tecnico-temporario");
     const $ = cheerio.load(response.data);
     const countries = $('p').map((i, item) => ({
@@ -17,7 +17,7 @@ async function scrap() {
             texto = `De uma olhada\n\n${paragrafo}`
         }
     }
-    main(`${texto}`,"E-mail enviado com sucesso!!","OTT 9º Região Militar","hix_x@hotmail.com");
+    return texto;
 }
 
-scrap();
+module.exports = { Exercito };
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { main } = require('./mandarEmail');
 const { DOE } = require('./funcDoe');
 const { UFMS } = require('./funcUfms');
+const { Exercito } = require('./funcExercito');
 
 const ano = new Date().getFullYear().toString();
 
 async function run() {
     const documentoGeradoDOE = await DOE('Klayton Chrysthian Oliveira Dias');
     const documentoGeradoUFMS = await UFMS();
+    const documentoGeradoExercito = await Exercito();
 
     const corpoEmail = `
     <html>
@@ -15,6 +17,8 @@ async function run() {
         <p>Aqui estão as análises solicitadas:</p>
         <p><strong>Movimentação Interna e Reingresso UFMS ${ano}</strong></p>
         <p>${documentoGeradoUFMS}</p>
+        <p><strong>Oficial Técnico Temporário (OTT) - PROCESSO SELETIVO ${ano}</strong></p>
+        <p>${documentoGeradoExercito}</p>
         <p>${documentoGeradoDOE}</p>
         <p>Por favor, mantenha-se informado sobre possíveis atualizações.</p>
         <p>Atenciosamente,</p>
