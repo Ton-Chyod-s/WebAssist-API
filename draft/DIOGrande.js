91f18ff4e3874295980dadb0096b9ffb1be7da75
Klay <hix_x@hotmail.com>
2024-04-21 08:31:11 -0400

    doc: att

---
+++ draft/DIOGrande.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+async function DIOGrande() {
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://diogrande.campogrande.ms.gov.br/');
+
+    await page.type('[id="searchPalavra"]', 'Klayton Chrysthian Oliveira Dias');
+
+    const botaoBuscar = await page.evaluateHandle(() => {
+        const xpath = '//*[@id="BotaoBuscarForm"]/span/span[2]';
+        const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
+        return result.singleNodeValue;
+    });
+    
+    await botaoBuscar.click();
+    
+
+}
+
+module.exports = { DIOGrande };
