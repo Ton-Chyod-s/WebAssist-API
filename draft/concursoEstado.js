a9d4b2ccc7228245b8a5d7dd24b47d8bc6324161
Klay <hix_x@hotmail.com>
2024-04-22 20:29:27 -0400

    doc: att

---
+++ draft/concursoEstado.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function seges() {
+    const response = await axios.get("http://www2.concursos.ms.gov.br/?location=editais");
+    const $ = cheerio.load(response.data);
+    const cards = $('a').map((i, item) => ({
+        texto: $(item).text().trim()
+    })).get();
+    for (let i = 0; i < cards.length; i++) {
+        if (cards[i].texto.includes('PROCESSO SELETIVO SIMPLIFICADO') || cards[i].texto.includes('Processo Seletivo Simplificado')) {
+            console.log(cards[i].texto)
+        }
+    }
+}
+
+seges()
