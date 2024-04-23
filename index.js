0bbc2a720b03887f4df9d7a3de620d1500e5682a
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-23 07:35:58 -0400

    doc: att

--- index.js
+++ index.js
@@ -55,6 +55,6 @@ module.exports = { run }
 
 if (require.main === module) {
     run("Klayton Chrysthian Oliveira Dias", "hix_x@hotmail.com");
-    // run("Silvianny Aparecida Faria Camilo", "silvianny.faria@ufms.br", false);
+    run("Silvianny Aparecida Faria Camilo", "silvianny.faria@ufms.br", false);
 }
 
