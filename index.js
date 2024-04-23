2fc5b88fd6a52145912bb552d1fe9025939d96a7
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-04-23 16:52:15 -0400

    doc: att

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+const { DOE } = require('./src/funcDoe');
 const express = require('express');
 const server = express();
 const PORT = 3000;
 
 // nodemon index.js
 
-server.get('/', (req,res) => {
-    
-    text = {
-        ' ': 'Api WebScrap'
+server.get('/DOE/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        let documentoGeradoDOE = await DOE(id);
+        return res.json(documentoGeradoDOE);
+    } catch (error) {
+        // Handle any errors that may occur during the asynchronous operation
+        return res.status(500).json({ error: 'An error occurred while fetching data' });
     }
-    return res.json(text)
-})
+});
 
 server.listen(PORT, () => {
     console.log('Servidor está funcionando!')
