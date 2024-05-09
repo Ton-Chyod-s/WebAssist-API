463064be0ff19268c5a3c687f8435175a7615c47
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-09 14:12:46 -0400

    doc: att

--- src/funcFiems.js
+++ src/funcFiems.js
@@ -1,15 +1,20 @@
-const { axios } = require('./axios');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const ano = new Date().getFullYear().toString();
 
 async function fiems() {
-    let documento = "";
-    const response = await axios.get('https://www.fiems.com.br/');
-
-    documento += `<p>FIEMS</p>`;
-    documento += `<p>${response.data}</p>`;
-
-    return documento;
+    try {
+        const response = await axios.get('https://www.fiems.com.br/trabalhe-conosco');
+        const $ = cheerio.load(response.data);
+        const jobDetails = $('.classe-do-elemento').map((i, item) => ({
+          texto: $(item).text().trim()
+        })).get();
+    
+        console.log(jobDetails);
+      } catch (error) {
+        return 'Ocorreu um erro ao fazer a solicitação:', error;
+      }
 }
 
 module.exports = { fiems };
