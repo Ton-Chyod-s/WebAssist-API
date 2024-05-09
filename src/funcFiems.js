b3c2fe61f7cbac5703fb567f19ca6e2ad2e3f7eb
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-05-09 14:43:25 -0400

    doc: att

--- src/funcFiems.js
+++ src/funcFiems.js
@@ -1,17 +1,36 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const https = require('https');
 
 const ano = new Date().getFullYear().toString();
+// não recomendado em produção por falha de segurança
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
 async function fiems() {
+    let resposta = ""
     try {
-        const response = await axios.get('https://www.fiems.com.br/trabalhe-conosco');
+        const response = await axios.get('https://www.fiems.com.br/trabalhe-conosco', { httpsAgent });
         const $ = cheerio.load(response.data);
-        const jobDetails = $('.classe-do-elemento').map((i, item) => ({
+        const jobDetails = $('div[class="col-md-4"]').map((i, item) => ({
           texto: $(item).text().trim()
         })).get();
-    
-        console.log(jobDetails);
+        
+        for (let i = 0; i < jobDetails.length; i++) {
+            const dicionario = jobDetails[i].texto.split('\n');
+            let cargoDicionario = new Array();
+            for (let i = 0; i < dicionario.length; i++) {
+                cargoDicionario.push(dicionario[i].trim())
+            }
+            
+            const cargo =  cargoDicionario[0]
+            const cidade =  cargoDicionario[1]
+            const local = cargoDicionario[2]
+            
+            if (cidade.includes('Campo Grande')) {
+                resposta += `${cargo}<br>${cidade}<br>${local}<br><br>`
+            }
+        }   
+        return resposta
       } catch (error) {
         return 'Ocorreu um erro ao fazer a solicitação:', error;
       }
