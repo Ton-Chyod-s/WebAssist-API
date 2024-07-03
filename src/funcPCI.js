066182c50f5e3486034b790373610a174ce3a62f
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-03 08:23:33 -0400

    doc: att

--- src/funcPCI.js
+++ src/funcPCI.js
@@ -3,10 +3,14 @@ const cheerio = require('cheerio');
 
 const funcPCI = async () => {
     let concurso = "";
-    const siteUrl = 'https://www.pciconcursos.com.br/concursos/centrooeste/';
+    // const siteUrl = 'https://www.pciconcursos.com.br/concursos/centrooeste/';
+    const siteUrl = 'https://www.pciconcursos.com.br';
     const data = await axios.get(siteUrl);
     const $ = cheerio.load(data);
     
+    console.log('Buscando concursos...');
+    console.log($);
+
     let title = $('div[id="concursos"]').text();
     title = title.split('\n').map(line => line.trim()).filter(line => line !== '');
     for (let i = 0; i < title.length; i++) {
