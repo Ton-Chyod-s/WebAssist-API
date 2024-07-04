81226c0159d2528fe9074fade3e9a007fc36fb11
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-04 08:21:18 -0400

    doc: att

--- src/funcExercito.js
+++ src/funcExercito.js
@@ -2,7 +2,8 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-let texto = "Aguardando..."
+let dictAnalise = {};
+let dictCorpo = {};
 
 async function Exercito() {
     try{
@@ -11,16 +12,38 @@ async function Exercito() {
         const countries = $('p').map((i, item) => ({
             texto: $(item).text().trim()
         })).get();
+     
         for (let i = 0; i < countries.length; i++) {
             const anoAtual = new Date().getFullYear().toString();
+            
             const paragrafo = countries[i]['texto']
             if (paragrafo.includes(anoAtual)) {
-                texto = `<h4>Prepare-se e leia atentamente o edital da convocação. Boa sorte, guerreiro!</h4>\n\n${paragrafo}`
+
+                dictAnalise[anoAtual] = `Prepare-se e leia atentamente o edital da convocação. Boa sorte, guerreiro!`
+                
+                const tituloAno = `ott-${anoAtual}`
+                const site = `https://9rm.eb.mil.br/index.php/${tituloAno}`
+
+                const responseCorpo = await axios.get(site);
+                const $Corpo = cheerio.load(responseCorpo.data);  
+                const countriesCorpo = $Corpo('td').map((i, item) => ({
+                    texto: $(item).text().trim()
+                })).get();
+
+                for (let i = 0; i < countriesCorpo.length; i++) {
+                    const conteudoLinha = countriesCorpo[i]['texto']
+                    if ( !conteudoLinha.includes(' MB') ) {
+                        dictCorpo[i] = conteudoLinha;
+                    }
+                }
+
+                dictAnalise[paragrafo] = dictCorpo
+
             }
         }
-        return texto;
+        return dictAnalise;
     } catch (error) {
-        return error;
+        return dictAnalise;
     }
 }
 
