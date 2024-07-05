8111e75e648eaf0ea4820c275b1cb98f0b3f3e05
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-05 14:29:42 -0400

    doc: att

--- src refactore/funcDioGrande.js
+++ src refactore/funcDioGrande.js
@@ -3,25 +3,51 @@ const cheerio = require('cheerio');
 
 async function funcDioGrande(nome) {
     nome = nome.replace(/\s/g, '%20').toUpperCase();
-    
-    const response = await axios.get(`https://diogrande.campogrande.ms.gov.br/edicoes/?palavra=` + nome + `&numero=&de=&ate=`);
-
+    const site = `https://diogrande.campogrande.ms.gov.br/edicoes/?palavra=${nome}&numero=&de=&ate=`;
+    const response = await axios.get(site);
     const $ = cheerio.load(response.data);
-    
-    console.log($);
 
-    const cards = $('tr').map((i, item) => ({
-        texto: $(item).text().trim()
-    })).get();
+    let cont = 0;
+
+    while (true) {
+        
+        const cards = $('table[id="SearchTableDiogrande"]').map((i, item) => ({
+            texto: $(item).text().trim()
+        })).get();
+
+        const ultimaPosicao = cards.length - 1;
+
+        if ( ultimaPosicao === 0 ) {
+            continue;
+        } else {
+            cont = 1;
+        }
+
+        if (cont !== 0) {
+            break;
+        }
+            
+    }
+    
+    
 
     console.log(cards);
- 
-}
 
-if (require.main === module) {
-    async function test() {
-        const result = await funcDioGrande('silvianny aparecida faria camilo');
-        console.log(result);
-    }
-    test()
-}
+
+}   
+
+
+
+
+const lohahahal = fetch('https://diogrande.campogrande.ms.gov.br/edicoes/?palavra=silvianny%20aparecida%20faria%20camilo&numero=&de=&ate=')
+
+
+const lol = 'lol'
+
+// if (require.main === module) {
+//     async function test() {
+//         const result = await funcDioGrande('silvianny aparecida faria camilo');
+//         console.log(result);
+//     }
+//     test()
+// }
