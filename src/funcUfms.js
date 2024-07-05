e4f6f9401a09a70d98b304aa38ee7ffb5246d198
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-05 10:00:49 -0400

    doc: att

--- src/funcUfms.js
+++ src/funcUfms.js
@@ -8,6 +8,8 @@ let dictUfms = new Object();
 let dictUfms1Sem = new Object();
 let dictUfms2Sem = new Object();
 
+const anoAtual = new Date().getFullYear().toString();
+
 async function UFMS() {
     try {
         const response = await axios.get("https://ingresso.ufms.br/publicacao/movimentacao-interna/");
@@ -26,14 +28,9 @@ async function UFMS() {
             const dictCountries2 = limpo.filter((item) => item !== '');
             const semestre = countries[i].texto.split(' – ')[1].split(' ')[0];
 
-            if (semestre.includes("1")) {
-                console.log('1º semestre')
-            }
-
             const anoSemestre = countries[i].texto.split(' – ')[1].split('\n')[0].split(' ')[3];
             const condicao = dictCountries2[1];
         
-
             if (anoSemestre === ano) {
                 if (semestre.includes("1")) {
                     if (condicao === "CONCLUÍDO") {
@@ -43,6 +40,22 @@ async function UFMS() {
                         };
 
                     } else {
+                        let dataPubli;
+                        let publicacao;
+
+                        for (let i = 0; i < dictCountries2.length; i++) {
+                            const element = dictCountries2[i].split(' ');
+                            if ( element.length === 6 ) {
+                                dataPubli = dictCountries2[i]  
+                            
+                            } else if (dictCountries2[i] != 'EM ANDAMENTO') {
+                                publicacao = dictCountries2[i]
+                                if ( !publicacao.includes('  Ver mais Movimentação Interna') && !publicacao.includes(`Movimentação Interna e Reingresso – 1º Semestre de ${anoAtual}`)) {
+                                    dictUfms1Sem[`${dataPubli} ${i}`] = publicacao
+                                }  
+                            }
+                        }
+
                         dictUfms[dictCountries2[0]] = dictCountries2;  
                     }
                 } else if (semestre.includes("2")) {
@@ -58,24 +71,20 @@ async function UFMS() {
                         let publicacao;
 
                         for (let i = 0; i < dictCountries2.length; i++) {
-
                             const element = dictCountries2[i].split(' ');
-
                             if ( element.length === 6 ) {
-                               
-                                dataPubli = dictCountries2[i]
-                                
+                                dataPubli = dictCountries2[i]  
                             
                             } else if (dictCountries2[i] != 'EM ANDAMENTO') {
                                 publicacao = dictCountries2[i]
-
-                                dictUfms2Sem[`${dataPubli} ${i}`] = publicacao
+                                if ( !publicacao.includes('  Ver mais Movimentação Interna') && !publicacao.includes(`Movimentação Interna e Reingresso – 2º Semestre de ${anoAtual}`)) {
+                                    dictUfms2Sem[`${dataPubli} ${i}`] = publicacao
+                                }  
                             }
-                            
-
                         }
                         
-                        dictUfms[dictCountries2[0]] = dictCountries2;
+
+                        dictUfms[dictCountries2[0]] = dictUfms2Sem;
                     }
                 }
 
