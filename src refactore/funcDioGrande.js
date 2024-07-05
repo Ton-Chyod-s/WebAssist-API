55efc58a607307807a28461f5613e760ef4e785a
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-05 16:57:22 -0400

    doc: att

--- src refactore/funcDioGrande.js
+++ src refactore/funcDioGrande.js
@@ -1,48 +1,41 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
+// const axios = require('axios');
+// const cheerio = require('cheerio');
 
-async function funcDioGrande(nome) {
-    nome = nome.replace(/\s/g, '%20').toUpperCase();
-    const site = `https://diogrande.campogrande.ms.gov.br/edicoes/?palavra=${nome}&numero=&de=&ate=`;
-    const response = await axios.get(site);
-    const $ = cheerio.load(response.data);
+// async function funcDioGrande(nome) {
+//     nome = nome.replace(/\s/g, '%20').toUpperCase();
+//     const site = `https://diogrande.campogrande.ms.gov.br/edicoes/?palavra=${nome}&numero=&de=&ate=`;
+//     const response = await request.get(site);
 
-    let cont = 0;
+//     let cont = 0;
 
-    while (true) {
-        
-        const cards = $('table[id="SearchTableDiogrande"]').map((i, item) => ({
-            texto: $(item).text().trim()
-        })).get();
+//     while (true) {
+//         const $ = cheerio.load(response.data);
+//         const cards = $('iframe').map((i, item) => ({
+//             texto: $(item).text().trim()
+//         })).get();
 
-        const ultimaPosicao = cards.length - 1;
+//         console.log(cards.length);
 
-        if ( ultimaPosicao === 0 ) {
-            continue;
-        } else {
-            cont = 1;
-        }
+//         const ultimaPosicao = cards.length - 1;
 
-        if (cont !== 0) {
-            break;
-        }
+//         if ( ultimaPosicao === -1 ) {
+//             continue;
+//         } else {
+//             cont = 1;
+//         }
+
+//         if (cont !== 0) {
+//             break;
+//         }
             
-    }
+//     }
     
-    
-
-    console.log(cards);
+//     console.log(cards);
 
 
-}   
+// }   
 
 
-
-
-const lohahahal = fetch('https://diogrande.campogrande.ms.gov.br/edicoes/?palavra=silvianny%20aparecida%20faria%20camilo&numero=&de=&ate=')
-
-
-const lol = 'lol'
 
 // if (require.main === module) {
 //     async function test() {
@@ -51,3 +44,42 @@ const lol = 'lol'
 //     }
 //     test()
 // }
+
+
+
+
+// // Definindo a URL para onde os dados serão enviados
+// const url = 'https://www.spdo.ms.gov.br/diariodoe';
+
+// // Definindo os dados que serão enviados
+// const data = { nome: 'silvianny aparecida faria camilo' };
+
+// // Fazendo a requisição POST
+// axios.post(url, data)
+//   .then(function (response) {
+//     // Carregar o HTML da resposta com cheerio
+//     const $ = cheerio.load(response.data);
+//     cont = 0;
+
+//     while (true) {
+//         // Mapear os elementos span e extrair o texto
+//         const cards = $('tr').map((i, item) => ({
+//             texto: $(item).text().trim()
+//         })).get();
+    
+//         console.log(cards);
+
+//         if ( cont === 1 ) {
+//             break;
+//         }
+
+
+//     }
+//   })
+//   .catch(function (error) {
+//     console.error('Error sending data:', error);
+//   });
+
+// const lol = 'lol';
+
+
