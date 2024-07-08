8c4266cd95559cdfbcdf5342321a7d61894579d3
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-08 10:39:18 -0400

    doc: att

--- src refactore/api-pci.js
+++ src refactore/api-pci.js
@@ -4,22 +4,43 @@ const cheerio = require('cheerio');
 json = new Object();
 analysis = new Object();
 
+function wait(time) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+}
+
 async function exam_region(source_code, region) {
     let initial_tag;
+    let marcacao;
 
-    function estadoProcura(estado) {
-        initial_tag = $(`div[id="${estado}"]`).map((i, item) => ({
-            texto: $(item).text().split("\n") 
-        })).get();
+    region = region.toUpperCase()
 
-        console.log(initial_tag)
+    if ( region === 'NACIONAL' ) {
+        marcacao = 'h2'
+    } else {
+        marcacao = 'div'
     }
 
-    const UFS = ['nacional','CE','SP','RJ','MG','ES','PR','SC','DF','GO','MS','MT','AM','AC','PA','RO','TO','AL','BA','MA','PA', 'PE','PI','RN','SE'
-    ]
+    function estadoProcura(estado) {
+        // initial_tag = $(`div[id="${estado}"]`)
 
+        // for (let i = 0; i < initial_tag.length; i++) {
+        //     console.log(initial_tag[i].children[0].children[0].data)
+        // }
+
+        // initial_tag = $(`div[class="ca"]`)
+
+        initial_tag = source_code.indexOf('<h2>NACIONAL</h2>') + '<h2>NACIONAL</h2>'.length;
+        final_tag = source_code.indexOf('<h2>REGIÃO SUDESTE</h2>') + '<h2>REGIÃO SUDESTE</h2>'.length;
+    }
+
+    
     const response = await axios.get(source_code);
     const $ = cheerio.load(response.data);
+    wait(1000)
 
     estadoProcura(region)
     
@@ -32,4 +53,4 @@ LINK = "https://www.pciconcursos.com.br/concursos/"
 
 
 
-exam_region(LINK, 'MS')
+exam_region(LINK, 'sp')
