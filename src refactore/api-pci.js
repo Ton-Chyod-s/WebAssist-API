be25c84be3424080fbca381a0dd0399345d206b8
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-08 09:48:37 -0400

    DOC: ATT

--- src refactore/api-pci.js
+++ src refactore/api-pci.js
@@ -1,14 +1,35 @@
-
-
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 json = new Object();
 analysis = new Object();
 
+async function exam_region(source_code, region) {
+    let initial_tag;
 
-function exam_region(source_code, region) {
+    function estadoProcura(estado) {
+        initial_tag = $(`div[id="${estado}"]`).map((i, item) => ({
+            texto: $(item).text().split("\n") 
+        })).get();
+
+        console.log(initial_tag)
+    }
+
+    const UFS = ['nacional','CE','SP','RJ','MG','ES','PR','SC','DF','GO','MS','MT','AM','AC','PA','RO','TO','AL','BA','MA','PA', 'PE','PI','RN','SE'
+    ]
+
+    const response = await axios.get(source_code);
+    const $ = cheerio.load(response.data);
+
+    estadoProcura(region)
     
 
 
 
-
 }
+
+LINK = "https://www.pciconcursos.com.br/concursos/"
+
+
+
+exam_region(LINK, 'MS')
