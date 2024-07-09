64294e537b048d093e9f18b62cc0e2846acfd3b9
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-09 15:49:23 -0400

    doc: att

--- src/funcConcursoEstado.js
+++ src/funcConcursoEstado.js
@@ -2,24 +2,25 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function concursoEstado() {
-    const site = 'http://www2.concursos.ms.gov.br/?location=editais <br><br>'
-    let resposta = ""
-
     const response = await axios.get("http://www2.concursos.ms.gov.br/?location=editais");
-    const $ = cheerio.load(response.data);
+    const site = response.data
 
+    initial_tag = site.indexOf(`<span class="formataTitulos"><br>"NOVO"</span>`) + `<span class="formataTitulos">"NOVO"</span>`.length;
     
-    const cards = $('a').map((i, item) => ({
+    final_tag = site.indexOf(`<span class="formataTitulos">EM ANDAMENTO</span>`) + `<span class="formataTitulos">EM ANDAMENTO</span>`.length;
+
+    const concursos_tag = site.slice(initial_tag, final_tag);
+    const $ = cheerio.load(concursos_tag)
+
+    const cards = $('span[class="formataCampos"] a').map((i, item) => ({
         texto: $(item).text().trim()
     })).get();
 
 
-    for (let i = 0; i < 13; i++) {
-        if (cards[i].texto.includes('PROCESSO SELETIVO SIMPLIFICADO') || cards[i].texto.includes('Processo Seletivo Simplificado') || cards[i].texto.includes('CONCURSO') || cards[i].texto.includes('Concurso')) {
-            resposta += `${cards[i].texto}<br><br>`
-        }
-    }
-    // return `<strong>Site: </strong>${site + resposta}`
+    console.log(cards)
+
+
+    
 
 
 }
