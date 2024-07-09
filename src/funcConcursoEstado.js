02808baea23dba849d0469f646ad5468f0225245
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-09 15:34:54 -0400

    doc: att

--- src/funcConcursoEstado.js
+++ src/funcConcursoEstado.js
@@ -7,15 +7,21 @@ async function concursoEstado() {
 
     const response = await axios.get("http://www2.concursos.ms.gov.br/?location=editais");
     const $ = cheerio.load(response.data);
+
+    
     const cards = $('a').map((i, item) => ({
         texto: $(item).text().trim()
     })).get();
+
+
     for (let i = 0; i < 13; i++) {
         if (cards[i].texto.includes('PROCESSO SELETIVO SIMPLIFICADO') || cards[i].texto.includes('Processo Seletivo Simplificado') || cards[i].texto.includes('CONCURSO') || cards[i].texto.includes('Concurso')) {
             resposta += `${cards[i].texto}<br><br>`
         }
     }
-    return `<strong>Site: </strong>${site + resposta}`
+    // return `<strong>Site: </strong>${site + resposta}`
+
+
 }
 
 module.exports = { concursoEstado }
