b165b5388dd1bd0f70f18eee5598ebe536598e93
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-10 15:50:46 -0400

    doc: att

--- src/funcUfmsGeral.js
+++ src/funcUfmsGeral.js
@@ -2,7 +2,8 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function funcUfmsGeral() {
-
+    const response = await axios.get('https://www.ufms.br/');
+    const html = response.data;
 }
 
 module.exports = funcUfmsGeral;
