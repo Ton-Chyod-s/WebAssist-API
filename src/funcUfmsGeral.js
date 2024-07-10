88b46a18e08115a9b290b5537ba39e0338d4e0e4
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-10 15:49:31 -0400

    doc: att

---
+++ src/funcUfmsGeral.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function funcUfmsGeral() {
+
+}
+
+module.exports = funcUfmsGeral;
+
+if (require.main === module) {
+    (async () => {
+        console.log(await funcUfmsGeral());
+
+    })();
+}
