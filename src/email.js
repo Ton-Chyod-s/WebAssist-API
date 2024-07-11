fcc22bfaaca3a8f322e888fd4bd143cdaf139b6f
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-11 16:32:17 -0400

    doc: att

--- src/email.js
+++ src/email.js
@@ -306,8 +306,8 @@ module.exports = { run }
 
 if (require.main === module) {
     run("Klayton Chrysthian Oliveira Dias", "hix_x@hotmail.com");
-    // run("Silvianny Aparecida Faria Camilo", "silvianny.faria@ufms.br", false);
-    // run("Ronaldo dos Santos","ronaldo.stst@gmail.com",false)
-    // run('Andreza Gabriela Leão Alves','andrezagabrielaalves@gmail.com',false)
-    // run('delmar silva dias','sdiascx@hotmail.com')
+    run("Silvianny Aparecida Faria Camilo", "silvianny.faria@ufms.br", false);
+    run("Ronaldo dos Santos","ronaldo.stst@gmail.com",false)
+    run('Andreza Gabriela Leão Alves','andrezagabrielaalves@gmail.com',false)
+    run('delmar silva dias','sdiascx@hotmail.com')
 }
