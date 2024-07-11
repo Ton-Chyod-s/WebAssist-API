b8ff804abecf3029cfe239f6d20dcc2dd27d106f
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-11 10:09:25 -0400

    doc: att

--- src/funcDioGrande.js
+++ src/funcDioGrande.js
@@ -1,54 +1,59 @@
 const puppeteer = require('puppeteer');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+let diarioOficial = new Object();
+let diarioAtual = new Object();
 
 const ano = new Date().getFullYear().toString();
 
 async function DIOGrande(nome) {
-    let diarioOficial = ""
+
+    const NOME = nome.replace(/\s/g, "%20").toUpperCase();
+    const LINK = `https://diogrande.campogrande.ms.gov.br/edicoes/?palavra=${NOME}&numero=&de=&ate=`
+    diarioOficial['link'] = LINK;
+
     const browser = await puppeteer.launch({
-        headless: false,
+        // headless: false,
     });
     const page = await browser.newPage();
-    await page.goto('https://diogrande.campogrande.ms.gov.br/');
+    await page.goto(LINK);
+    let spansText;
+    let spansHref;
 
-    await page.type('[id="searchPalavra"]', nome);
-
-    const botaoBuscar = await page.evaluateHandle(() => {
-        const xpath = '//*[@id="BotaoBuscarForm"]/span/span[2]';
-        const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-        return result.singleNodeValue;
-    });
-    
-    await botaoBuscar.click();
-      
-    await page.waitForNavigation({ waitUntil: 'domcontentloaded' })
-    
     while (true) {
-        await page.waitForSelector('table > tbody > tr');
-        const spansText = await page.$$eval('table > tbody > tr', spans => spans.map(span => span.innerText));
-    
-        if (spansText.length > 1) {
+        // await page.waitForSelector('table > tbody > tr');
+        spansText = await page.$$eval('table > tbody > tr', spans => spans.map(span => span.innerText));
+
+        if ( spansText[0].includes('DOWNLOAD') ) {
             console.log("Tabela encontrada, analisando dados...");
             break;
         }
     }
     
-    await page.$$('#SearchTableDiogrande tbody > tr');
-        
-    const planilhaHTML = await page.$$eval('#SearchTableDiogrande tbody > tr', rows => rows.map(row => row.innerText));
-    for (let i = 0; i < planilhaHTML.length; i++) {
-        if (i !== "DOWLOAD") {
-            const planilhaSeparada = planilhaHTML[i].split("\t");
-            const anoDiario = planilhaSeparada[2].split('/')[2];
-            if (anoDiario == ano) {
-                const DIO = `${planilhaSeparada[0]} ${planilhaSeparada[1]} ${planilhaSeparada[2]}<br>`
-                diarioOficial += `<p>Nome:  ${nome} Ano:    ${ano}</p>`
-                diarioOficial += DIO;
-            } else if (diarioOficial === "") {
-                diarioOficial += `Nome:  ${nome} Ano:    ${ano}<br>`
-                diarioOficial += `<p>Lamento informar que não foram encontrados Diários Oficiais Digitais associados ao seu nome até a presente data.</p>`;
-            }         
-        } 
+    for (let i = 0; i < spansText.length; i++) {
+        const element = spansText[i].split('\t');
+        for (let j = 0; j < element.length; j++) {
+            if ( element[j].trim().includes(ano) ) {
+                const numero = element[0];
+                const tipo = element[1];
+                const data = element[2];
+                diarioAtual['tipo'] = tipo;
+                diarioAtual['data'] = data;
+
+                diarioOficial[numero] = diarioAtual;
+                diarioAtual = {};
+                break;
+            }
+        }
     }
+
+    if (Object.keys(diarioOficial).length === 1) {
+        diarioAtual['erro'] = 'Lamento informar que não foram encontrados Diários Oficiais Digitais associados ao seu nome até a presente data.';
+        diarioOficial['diario Oficial'] = diarioAtual;
+    }
+    
+
     await browser.close();
     return diarioOficial;
 }
@@ -57,7 +62,11 @@ module.exports = { DIOGrande };
 
 if (require.main === module) {
     (async () => {
-        const nome = "delmar silva dias";
+        // const nome = "delmar silva dias";
+        // const nome = "silvianny aparecida faria camilo"
+        const nome = "klayton chrysthian oliveira dias";
+        // const nome = "Andreza Gabriela Leão Alves";
+        // const nome = "Ronaldo dos Santos";
         const diario = await DIOGrande(nome);
         console.log(diario);
     })();
