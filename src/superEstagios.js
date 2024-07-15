77c03438ebe51d4014ac615345592b428a8926db
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-15 10:26:40 -0400

    doc: att

---
+++ src/superEstagios.js
@@ -0,0 +1,14 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+const LINK="https://www.superestagios.com.br/index/processoSeletivo/processo.php?v=MTky"
+
+async function superEstagios() {
+
+}
+
+module.exports = { superEstagios }
+
+if (require.main === module) {
+    
+}
