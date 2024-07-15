77bd0dda9c1c48464b7c20b76fdeb787e53be149
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-15 14:58:51 -0400

    doc: att

--- src/mandarEmail.js
+++ src/mandarEmail.js
@@ -1,18 +1,22 @@
 const nodemailer = require("nodemailer");
 
+require('dotenv').config({  
+    path: process.env.NODE_ENV !== "main" ? ".env" : ".env"
+  })
+
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
-        user: "perindevboot@gmail.com",
-        pass: "gxkqsyymnogquthd",
+        user: process.env.USER,
+        pass: process.env.PASS,
       },
     });
 
 async function main(texto,imprimirConsole,assunto,para) {
     await transporter.sendMail({
-        from: "perindevboot@gmail.com",
+        from: process.env.USER,
         to: para,
         subject: assunto,
         html: texto
