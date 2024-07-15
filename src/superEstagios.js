3c6ede169c3c228bea9940f8039dbe1ef81d484c
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-15 10:53:50 -0400

    doc: att

--- src/superEstagios.js
+++ src/superEstagios.js
@@ -3,8 +3,24 @@ const axios = require('axios');
 
 const LINK="https://www.superestagios.com.br/index/processoSeletivo/processo.php?v=MTky"
 
+let listProcess = new Array();
+
 async function superEstagios() {
     const response = await axios.get(LINK);
+    const $ = cheerio.load(response.data);
+    const cards = $('li').map((i, card) => {
+        const text = $(card).text().trim().replace('- Clique aqui para visualizar', '');
+        const link = $(card).find('a').attr('href');
+        return {"texto": text, "link": link};
+    }).get();
+
+    for (let i = 0; i < cards.length; i++) {
+        const element = cards[i].texto;
+        if ( !element.includes("O candidato") ) {
+            listProcess.push(cards[i]);
+        }
+    }
+    return console.log(listProcess);
 }
 
 module.exports = { superEstagios }
