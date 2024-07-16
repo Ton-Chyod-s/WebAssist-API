d6d66b1acdb713c854d279dc1d674116cbab1ce6
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-07-16 07:37:10 -0400

    doc: att

--- src/mandarEmail.js
+++ src/mandarEmail.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer");
 
 // se for true, o teste será feito, se for false, entrera em produção
-const on =  true;
+const on =  false;
 
 let arg = ".env";
 if ( on === true ) {
