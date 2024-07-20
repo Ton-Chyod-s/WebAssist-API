2084851b7387dd4293eabadb0c2b4b43d2c42edb
Klay <hix_x@hotmail.com>
2024-07-20 09:31:19 -0400

    doc: att

---
+++ src/func/funcSenac.js
@@ -0,0 +1,12 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+async function funcSenac() {
+
+}
+
+module.exports = { funcSenac }
+
+if (require.main === module) {
+    funcSenac(funcSenac)
+}
