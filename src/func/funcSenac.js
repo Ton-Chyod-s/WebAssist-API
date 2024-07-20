9aae975987a6adc11265ac7c9e3794d9e67f1b46
Klay <hix_x@hotmail.com>
2024-07-20 09:47:38 -0400

    doc: att

--- src/func/funcSenac.js
+++ src/func/funcSenac.js
@@ -1,7 +1,19 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
+const httpsAgent = new https.Agent({ rejectUnauthorized: false });
+
 async function funcSenac() {
+
+    const LINK_SITE = 'https://ms.senac.br/site/vagas'
+
+    const reponse = await axios.get(LINK_SITE, { httpsAgent });
+    const site = reponse.data;
+
+    initial_tag = site.indexOf(`<div class="col-md-12">`) + `<div class="col-md-12">`.length;
+
+    final_tag = site.indexOf(`<div class="col-md-4">`) + `<div class="col-md-4">`.length;
+
 
 }
 
