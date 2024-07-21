edc7b0188bcdc7f9f20fe2753af2a65a5b80cc15
Klay <hix_x@hotmail.com>
2024-07-21 11:27:47 -0400

    doc: att

--- src/func/funcSenac.js
+++ src/func/funcSenac.js
@@ -1,5 +1,7 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const https = require('https');
+const { link } = require('fs');
 
 const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
@@ -10,10 +12,30 @@ async function funcSenac() {
     const reponse = await axios.get(LINK_SITE, { httpsAgent });
     const site = reponse.data;
 
-    initial_tag = site.indexOf(`<div class="col-md-12">`) + `<div class="col-md-12">`.length;
+    initial_tag = site.indexOf(`<h5>Em Campo Grande</h5>`) + `<h5>Em Campo Grande</h5>`.length;
 
-    final_tag = site.indexOf(`<div class="col-md-4">`) + `<div class="col-md-4">`.length;
+    final_tag = site.indexOf(`<h5>Em Corumbá</h5>`) + `<h5>Em Corumbá</h5>`.length;
 
+    const concursos_tag = site.slice(initial_tag, final_tag);
+
+    const $ = cheerio.load(concursos_tag);
+
+    const cards = $('div').map((index, element) => {
+        const $element = $(element);
+        const text = $element.text().trim();
+        const link = $element.find('div#collapse743 .panel-body ul li a').attr('href');
+        
+        return { 
+            value: text, 
+            link: link 
+        };
+    }).get();
+
+    for (let i = 0; i < cards.length; i++) {
+        const element = cards[i];
+        console.log(element.value)
+    
+    }
 
 }
 
