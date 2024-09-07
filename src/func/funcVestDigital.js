3cc25a4f05c38997bed185bbf3e38a0e7e7626ef
Klay <hix_x@hotmail.com>
2024-09-07 07:54:58 -0400

    doc|: att

--- src/func/funcVestDigital.js
+++ src/func/funcVestDigital.js
@@ -2,25 +2,60 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const { func } = require('./func');
 
+const anoAtual = new Date().getFullYear();
+const anoPosterior = anoAtual + 1;
+
 async function vestDigital() {
-
     const site = "https://ingresso.ufms.br/processo/vestibular-ufms-digital/"
-
     let response;
 
     response = await axios.get(site);
     const $ = cheerio.load(response.data)
-    const cards = $().map(
+    const cards = $('div[class="btn-group"]').map(
         (i, item) => ({
-            texto: $(item).text().trim()
+            texto: $(item).text().trim().replace(/\t/g, '').replace(/\n/g, ''),
+            link: $(item).find('a').attr('href')
         })).get()
 
+    function cardAnoAnalisado (ano) {
+        const cardsAno = cards.filter((item) => item.texto.includes(ano)).map((item) => ({
+            texto: item.texto,
+            link: item.link
+        }));
+        return cardsAno;
+    }
+
+    const cardAnoAtual = cardAnoAnalisado(anoAtual);
+    const cardAnoPosterior = cardAnoAnalisado(anoPosterior);
+
+    if ( cardAnoPosterior.length != 0 ) {
+        console.log('Não há informações para o ano posterior.')
+    } else {
+        console.log(cardAnoAtual)
+    }
+
+
+
     console.log(cards)
+
+
+
+    // let cards = $('div[class="col-md-12"]').map(
+    //     (i, item) => ({
+    //         texto: $(item).text().trim().replace(/\t/g, '').replace(/\n/g, ''),
+    //         link: $(item).find('a').attr('href')
+    //     })).get()
+
+   
+    
+
+
+
     
 }
 
-modeule.exports = { vestDigital };
+
 
 if (require.main === module) {
-    func(vestDigital);
+    vestDigital();
 }
