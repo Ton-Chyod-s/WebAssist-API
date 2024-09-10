c326d8631094b8436fd01b3c8a2bbd588e02c36f
Klay <hix_x@hotmail.com>
2024-09-09 20:29:02 -0400

    doc: att

---
+++ src/teste.js
@@ -0,0 +1,20 @@
+const { vestDigital } = require('./func/funcVestDigital');
+
+(async function() {
+    let documentoUfmsGeral = await vestDigital();
+    console.log(documentoUfmsGeral)
+
+    for ( let i in documentoUfmsGeral ) {
+        const item = documentoUfmsGeral[i]
+        
+        if (typeof(item) !== 'string') {
+            for ( let linha in item ) {
+                listaUFMSGeral += `<p>${item[linha]}</p>`
+            }
+        } else {
+            listaUFMSGeral += `<h4>${item}</h4>`
+        }
+    }
+})();
+
+
